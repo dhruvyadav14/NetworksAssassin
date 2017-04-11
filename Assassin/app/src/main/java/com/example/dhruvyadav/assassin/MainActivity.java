c2543989a2498580ebbc3b92292fdad9baa12d00
Luke Anderson <Luke Anderson>
2017-04-11 16:57:30 -0500

    code v1

--- Assassin/app/src/main/java/com/example/dhruvyadav/assassin/MainActivity.java
+++ Assassin/app/src/main/java/com/example/dhruvyadav/assassin/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.dhruvyadav.assassin;
 
+import android.content.Intent;
+import android.os.Message;
+import android.os.Messenger;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        // Initialize the start button
+        Button startButton = (Button) findViewById(R.id.startButton);
+
+        // set OnClickListener to download image
+        // from URL via ImageIntentService
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setContentView(R.layout.startgame);
+            }
+        });
+
+        // Initialize the 'Download Thread' Button
+        Button continueButton = (Button) findViewById(R.id.continueButton);
+
+        // set OnClickListener to download image
+        // from URL via a DownloadImageHandler
+        continueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setContentView(R.layout.continuegame);
+            }
+        });
     }
 }
